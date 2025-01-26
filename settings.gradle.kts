4927e182bbfebe0591415aee1e5ba831bb560cac
SplinteeX <Jepsteri04@gmail.com>
2025-01-26 17:41:33 +0200

    Lab 3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,5 @@ rootProject.name = "MobiiliKHS_SM"
 include("untitled")
 include("src:main:Lab1")
 findProject(":src:main:Lab1")?.name = "Lab1"
+include("src:main:Lab3")
+findProject(":src:main:Lab3")?.name = "Lab3"
