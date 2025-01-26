8911d434f31195959ff5e48b891bcedc24e61182
SplinteeX <Jepsteri04@gmail.com>
2025-01-26 17:42:33 +0200

    Lab 3

---
+++ src/main/Lab3/src/main/kotlin/Complex.kt
@@ -0,0 +1,39 @@
+package org.example
+
+const val EPS = 0.0000001
+
+data class Complex(val real: Double, val imaginary: Double) {
+    constructor(real: Int, imaginary: Int) : this(real.toDouble(), imaginary.toDouble())
+
+    operator fun plus(other: Complex): Complex {
+        return Complex(this.real + other.real, this.imaginary + other.imaginary)
+    }
+
+    operator fun minus(other: Complex): Complex {
+        return Complex(this.real - other.real, this.imaginary - other.imaginary)
+    }
+
+    operator fun times(other: Complex): Complex {
+        val realPart = this.real * other.real - this.imaginary * other.imaginary
+        val imaginaryPart = this.real * other.imaginary + this.imaginary * other.real
+        return Complex(realPart, imaginaryPart)
+    }
+
+    val abs: Double
+        get() = kotlin.math.sqrt(real * real + imaginary * imaginary)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Complex) return false
+        return kotlin.math.abs(this.real - other.real) < EPS &&
+                kotlin.math.abs(this.imaginary - other.imaginary) < EPS
+    }
+
+    override fun hashCode(): Int {
+        return 31 * real.hashCode() + imaginary.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Complex(real=$real, imaginary=$imaginary)"
+    }
+}
